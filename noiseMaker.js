f55aa49176ada9ec892f07d6974eafde8a0c00b0
alex-mack-code <alex.mack.code@gmail.com>
2020-04-22 19:34:57 -0400

    button makes a sound

--- noiseMaker.js
+++ noiseMaker.js
@@ -4,11 +4,11 @@ window.onload = function()
 {
     let d = new Date();
     document.getElementById('testjavascript').innerHTML = "<h1>Today's date is " + d + "</h1>"
-
-
-
+    
     function myFunction() {
         document.getElementById("clicked").innerHTML = "YOU CLICKED ME!";
+        const synth = new Tone.Synth().toMaster();
+        synth.triggerAttackRelease("C4", "8n");
       }
     
     document.getElementById("myButton").onclick = function() {myFunction()};
