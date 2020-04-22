789fa4e5f31abe611cc75fa0ee386ebbf7fa4882
alex-mack-code <alex.mack.code@gmail.com>
2020-04-22 18:57:52 -0400

    button clicked

--- noiseMaker.js
+++ noiseMaker.js
@@ -4,8 +4,15 @@ window.onload = function()
 {
     let d = new Date();
     document.getElementById('testjavascript').innerHTML = "<h1>Today's date is " + d + "</h1>"
-}
 
+
+
+    function myFunction() {
+        document.getElementById("clicked").innerHTML = "YOU CLICKED ME!";
+      }
+    
+    document.getElementById("myButton").onclick = function() {myFunction()};
+}
 
 
 
